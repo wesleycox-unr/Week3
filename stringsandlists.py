539a47a6935a52545593930e40dfa8b40a93cff5
Wesley D Cox <wesleycox@unr.edu>
2019-09-12 20:04:52 -0700

    Initial commit

---
+++ stringsandlists.py
@@ -0,0 +1,17 @@
+# Strings and lists
+
+s = "This is a long string, oh boy"
+
+l = [1, 2, 3, 4]
+
+#for number in l:
+#    print(number)
+
+#for letter in s:
+#    print(letter) #new line character 
+
+l.append(121)
+print(l)
+
+s.append("France")
+print(s)
